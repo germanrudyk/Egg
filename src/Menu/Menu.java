3ae257107d5b622fe3f2307b7a6b4e1e5a0e0d97
German Rudyk <germanrudyk87@gmail.com>
2023-01-22 12:33:50 -0300

    Segundo Commit

---
+++ src/Menu/Menu.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Menu;
+
+import Servicios.LibroService;
+
+/**
+ *
+ * @author German
+ */
+public class Menu {
+
+    private final LibroService ls;
+    
+    public Menu() {
+        
+        this.ls = new LibroService();
+        
+    }
+    
+    
+    
+}
